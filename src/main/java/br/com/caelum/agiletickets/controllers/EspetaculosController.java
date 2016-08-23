32cc4f88909d8b13b4683fedb48b6374525bca13
Marcel Saraiva <mdfsaraiva@gmail.com>
2016-08-23 10:01:26 +0200

    Terceiro

--- src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
+++ src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
@@ -14,7 +14,6 @@ import br.com.caelum.agiletickets.domain.Agenda;
 import br.com.caelum.agiletickets.domain.DiretorioDeEstabelecimentos;
 import br.com.caelum.agiletickets.domain.precos.CalculadoraDePrecos;
 import br.com.caelum.agiletickets.models.Espetaculo;
-import br.com.caelum.agiletickets.models.Estabelecimento;
 import br.com.caelum.agiletickets.models.Periodicidade;
 import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.vraptor.Controller;
@@ -143,10 +142,4 @@ public class EspetaculosController {
 		validator.onErrorUse(status()).notFound();
 		return espetaculo;
 	}
-
-	// metodo antigo. aqui soh por backup
-	/*private Estabelecimento criaEstabelecimento(Long id) {
-		return estabelecimentos.todos().get(0);
-	}*/
-	
 }
